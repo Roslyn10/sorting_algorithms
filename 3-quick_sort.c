e0437d11f0b4a4915e46ce8ebd1cea614cb44b38
Roslyn10 <roslynclewis1004@gmail.com>
2024-03-05 22:38:23 +0200

    Adding quick sort codes

--- 3-quick_sort.c
+++ 3-quick_sort.c
@@ -1,13 +1,94 @@
 #include "sort.h"
 
 /**
+ * swap - A function that swaps integers in an array.
+ * Description - Swap two integers in an array.
+ * @q: The first integer to swap.
+ * @s: The second integer to swap.
+ */
+
+void swap(int *q, int *s)
+{
+	int temp;
+
+	temp = *q;
+	*q = *s;
+	*s = temp;
+}
+
+/**
+ * lomuto - Order a subset of an array of integers according to
+ *                    the lomuto partition scheme (last element as pivot).
+ * @array: The array of integers.
+ * @size: The size of the array.
+ * @left: The starting index of the subset to order.
+ * @right: The ending index of the subset to order.
+ *
+ * Return: Above
+ */
+
+int lomuto(int *array, size_t size, int left, int right)
+{
+	int *pivot, above, below;
+
+	pivot = array + right;
+	for (above = below = left; below < right; below++)
+	{
+		if (array[below] < *pivot)
+		{
+			if (above < below)
+			{
+				swap(array + below, array + above);
+				print_array(array, size);
+			}
+			above++;
+		}
+	}
+
+	if (array[above] > *pivot)
+	{
+		swap(array + above, pivot);
+		print_array(array, size);
+	}
+
+	return (above);
+}
+
+/**
+ * lomuto_sort - Implement the quicksort algorithm through recursion.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ * @left: The starting index of the array partition to order.
+ * @right: The ending index of the array partition to order.
+ *
+ * Description: Uses the Lomuto partition scheme.
+ */
+
+void lomuto_sort(int *array, size_t size, int left, int right)
+{
+	int part;
+
+	if (right - left > 0)
+	{
+		part = lomuto(array, size, left, right);
+		lomuto_sort(array, size, left, part - 1);
+		lomuto_sort(array, size, part + 1, right);
+	}
+}
+
+/**
  * quick_sort - A function that sorts an array of integers
  * in ascending order using the quick sort algorithm
  * Description - Sorts an array using the quick sort algorithm
- * @array:
- * @size:
+ * @array: The array of integrs
+ * @size: The size of the array
  * Return: Nothing
  */
 
 void quick_sort(int *array, size_t size)
 {
+	if (array == NULL || size < 2)
+		return;
+
+	lomuto_sort(array, size, 0, size - 1);
+}
--- sort.h
+++ sort.h
@@ -35,7 +35,10 @@ typedef enum bool_values
 /**Given code and prototypes**/
 void print_list(const listint_t *list);
 void print_array(const int *array, size_t size);
+void lomuto_sort(int *array, size_t size, int left, int right);
+int lomuto(int *array, size_t size, int left, int right);
 
+void quick_sort(int *array, size_t size);
 void swap(int *b, int *s);
 void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
